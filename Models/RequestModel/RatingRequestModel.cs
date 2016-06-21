5cdd1ef181a918585b5140bb78d97ea1fcd81256
Fisher Robison <Fisherrobison@gmail.com>
2016-06-21 08:24:50 -0700

    Update RatingRequestModel.cs

--- Models/RequestModel/RatingRequestModel.cs
+++ Models/RequestModel/RatingRequestModel.cs
@@ -3,7 +3,7 @@ using System.Collections.Generic;
 using System.Linq;
 using System.Web;
 
-namespace Sabio.Web.Models.Requests
+namespace Spright.Web.Models.Requests
 {
     public class RatingRequestModel
     {
